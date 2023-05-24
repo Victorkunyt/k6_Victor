baad0b030460595b697ee14153e3d334ea8fd03f
Your Name <you@example.com>
2023-05-24 11:22:49 -0300

    Commit

--- scripts/Stress.test.js
+++ scripts/Stress.test.js
@@ -1,27 +1,26 @@
 import { sleep, check } from "k6";
 import http from "k6/http";
-const ENV = 'https://reqres.in/api/'
+const ENV = "https://reqres.in/api/";
 
 export const options = {
 	stages: [
 		{ duration: "30s", target: 3 }, // Abaixo da carga Normal
-		{ duration: "1m", target: 3 },
+		{ duration: "1m", target: 3 }, //
 		{ duration: "30s", target: 5 }, // Carga Load
-		{ duration: "1m", target: 5 },
+		{ duration: "1m", target: 5 }, //
 		{ duration: "30s", target: 7 }, // Perto do ponto de Stress
-		{ duration: "1m", target: 7 },
+		{ duration: "1m", target: 7 }, //
 		{ duration: "30s", target: 10 }, // Além do ponto de Stress
-		{ duration: "1m", target: 10 },
+		{ duration: "1m", target: 10 }, //
 		{ duration: "1m", target: 1 }, // ramp-down, Estágio de recuperação
 	],
 	thresholds: {
-		http_req_failed: ["rate<0.01"],
+		http_req_failed: ["rate<0.01"], // Taxa de falha menor que 1%
 		http_req_duration: ["p(95)<500"], // 95% das requisições devem ser abaixo de 500ms
 	},
 };
 
 export default function () {
-	
 	let res = http.get(`${ENV}users?page=2`);
 
 	check(res, {
